7895ca238440ff50961a0fb0f914787dba428001
Marek Bazylewicz <mbazyl2@gmail.com>
2017-06-10 19:18:51 +0200

    Finished lecture 3 out of 12 from the course. Added some kind of movement/annimation.

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 var canvas;
 var canvasContext;
+var ballX = 10;
+var ballY = 10;
 
 window.onload = function() {
 	console.log("Hey");
 	canvas = document.getElementById('gameCanvas');
 	canvasContext = canvas.getContext('2d');
+
+	setInterval(drawEverthing, 100);
+
+}
+
+function drawEverthing() {
+	console.log("ballX value: "+ballX);
+	if (ballX < 800) {
+	ballX += 5;
+	}
+
 	canvasContext.fillStyle = 'black';
 	canvasContext.fillRect(0,0,canvas.width, canvas.height);
+	
 	canvasContext.fillStyle = 'red';
-	canvasContext.fillRect(100,200, 50, 100);
-	canvasContext.fillStyle = 'green';
-	canvasContext.fillRect(230,230, 10, 10);
+	canvasContext.fillRect( ballX, ballY, 10, 10);
+	
 	canvasContext.fillStyle = 'white';
 	canvasContext.fillRect(300,200, 200, 200);
-
 }
